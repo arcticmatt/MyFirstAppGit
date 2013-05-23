ad97cbc868ac292cf82b41a19c37fbc451472e99
arcticmatt <mlimcollege@gmail.com>
2013-05-22 19:28:47 -0700

    modified the OnCreate message to send a text when the button btnSendSMS is clicked

--- MyFirstAppGit/src/com/example/myfirstappgit/MainActivity.java
+++ MyFirstAppGit/src/com/example/myfirstappgit/MainActivity.java
@@ -5,16 +5,39 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
     public final static String EXTRA_MESSAGE = "com.example.myfirstappgit.MESSAGE";
+    Button btnSendSMS;
+    EditText txtPhoneNo;
+    EditText txtMessage;
 
     @Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+        btnSendSMS = (Button) findViewById(R.id.btnSendSMS);
+        txtPhoneNo = (EditText) findViewById(R.id.txtPhoneNo);
+        txtMessage = (EditText) findViewById(R.id.txtMessage);
+
+        btnSendSMS.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String phoneNo = txtPhoneNo.getText().toString();
+                String message = txtMessage.getText().toString();
+                if (phoneNo.length()>0 && message.length()>0) {
+                    sendSMS(phoneNo, message);
+                }
+                else
+                    Toast.makeText(getBaseContext(),
+                    "Please enter both phone number and message.",
+                    Toast.LENGTH_SHORT).show();
+            }
+        })
 	}
 
 	@Override
@@ -26,7 +49,7 @@ public class MainActivity extends Activity {
 
     public void sendMessage (View view) {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.edit_message);
+        EditText editText = (EditText) findViewById(R.id.txtMessage);
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);

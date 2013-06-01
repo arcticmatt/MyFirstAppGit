a939a45985d257c2cc5babcde1ffa8c45f6bdbd5
arcticmatt <mlimcollege@gmail.com>
2013-05-31 23:25:14 -0700

    IT WORKS
    -listview shows all received texts

--- MyFirstAppGit/src/com/example/myfirstappgit/CursorLoaderListFragment.java
+++ MyFirstAppGit/src/com/example/myfirstappgit/CursorLoaderListFragment.java
@@ -48,7 +48,7 @@ public class CursorLoaderListFragment extends ListFragment
 
 
 
-        Uri mSmsinboxQueryUri = Uri.parse("content://sms/inbox");
+        /*Uri mSmsinboxQueryUri = Uri.parse("content://sms/inbox");
         Cursor cursor1 = getActivity().getContentResolver().query(mSmsinboxQueryUri,
                 new String[] { "_id", "thread_id", "address", "person", "date",
                         "body", "type" }, null, null, null);
@@ -64,12 +64,12 @@ public class CursorLoaderListFragment extends ListFragment
                 String msg = cursor1.getString(cursor1.getColumnIndex(columns[3]));
                 String type = cursor1.getString(cursor1.getColumnIndex(columns[4]));
             }
-        }
+        }*/
 
         // Create an empty adapter we will use to display the loaded data.
         mAdapter = new SimpleCursorAdapter(getActivity(),
                 android.R.layout.simple_list_item_2, null,
-                new String[] { Contacts.DISPLAY_NAME, Contacts.CONTACT_STATUS },
+                new String[] { "_id", "body" },
                 new int[] { android.R.id.text1, android.R.id.text2 }, 0);
         setListAdapter(mAdapter);
 
@@ -120,7 +120,7 @@ public class CursorLoaderListFragment extends ListFragment
         Log.i("FragmentComplexList", "Item clicked: " + id);
     }
 
-    // These are the Contacts rows that we will retrieve.
+    /*// These are the Contacts rows that we will retrieve.
     static final String[] CONTACTS_SUMMARY_PROJECTION = new String[] {
             Contacts._ID,
             Contacts.DISPLAY_NAME,
@@ -128,29 +128,41 @@ public class CursorLoaderListFragment extends ListFragment
             Contacts.CONTACT_PRESENCE,
             Contacts.PHOTO_ID,
             Contacts.LOOKUP_KEY,
-    };
+    };*/
 
     public Loader<Cursor> onCreateLoader(int id, Bundle args) {
         // This is called when a new Loader needs to be created.  This
         // sample only has one Loader, so we don't care about the ID.
         // First, pick the base URI to use depending on whether we are
         // currently filtering.
-        Uri baseUri;
+        /*Uri baseUri;
         if (mCurFilter != null) {
             baseUri = Uri.withAppendedPath(Contacts.CONTENT_FILTER_URI,
                     Uri.encode(mCurFilter));
         } else {
             baseUri = Contacts.CONTENT_URI;
-        }
+        }*/
+
+
+        Uri mSmsinboxQueryUri = Uri.parse("content://sms/inbox");
+        String[] columns = new String[] { "_id", "body" };
+
+
+
+
 
         // Now create and return a CursorLoader that will take care of
         // creating a Cursor for the data being displayed.
-        String select = "((" + Contacts.DISPLAY_NAME + " NOTNULL) AND ("
+        String select1 = "((" + Contacts.DISPLAY_NAME + " NOTNULL) AND ("
                 + Contacts.HAS_PHONE_NUMBER + "=1) AND ("
                 + Contacts.DISPLAY_NAME + " != '' ))";
-        return new CursorLoader(getActivity(), baseUri,
-                CONTACTS_SUMMARY_PROJECTION, select, null,
-                Contacts.DISPLAY_NAME + " COLLATE LOCALIZED ASC");
+
+
+        String someValue = Contacts.DISPLAY_NAME;
+        String select = "((" + "_id" + "=+19254706288))";
+        return new CursorLoader(getActivity(), mSmsinboxQueryUri,
+                columns, null, null,
+                null);
     }
 
     public void onLoadFinished(Loader<Cursor> loader, Cursor data) {

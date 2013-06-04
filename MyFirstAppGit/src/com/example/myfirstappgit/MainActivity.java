32a4b2918f6c4e4974ded46ae88358a71dc7e1bd
arcticmatt <mlimcollege@gmail.com>
2013-06-04 00:59:32 -0700

    Copy code from StackOverflow. Code works.
    -http://stackoverflow.com/questions/12400504/selecting-contact-from-autocomplete-textview

--- MyFirstAppGit/src/com/example/myfirstappgit/MainActivity.java
+++ MyFirstAppGit/src/com/example/myfirstappgit/MainActivity.java
@@ -31,6 +31,8 @@ import android.view.ViewGroup;
 import android.widget.*;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class MainActivity extends Activity {
@@ -43,6 +45,10 @@ public class MainActivity extends Activity {
 
     private TextView textCount;
 
+    private ArrayList<Map<String, String>> mPeopleList;
+    private SimpleAdapter mAdapter;
+    private AutoCompleteTextView mTxtPhoneNo;
+
 
 
     @Override
@@ -54,6 +60,33 @@ public class MainActivity extends Activity {
         //txtPhoneNo = (EditText) findViewById(R.id.txtPhoneNo);
         txtMessage = (EditText) findViewById(R.id.txtMessage);
         textCount = (TextView) findViewById(R.id.charCounter);
+
+
+        mPeopleList = new ArrayList<Map<String, String>>();
+        PopulatePeopleList();
+        mTxtPhoneNo = (AutoCompleteTextView) findViewById(R.id.mmWhoNo);
+
+        mTxtPhoneNo.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> av, View arg1, int index,
+                                    long arg3) {
+                Map<String, String> map = (Map<String, String>) av.getItemAtPosition(index);
+
+                String name  = map.get("Name");
+                String number = map.get("Phone");
+                mTxtPhoneNo.setText(""+name+"<"+number+">");
+
+            }
+
+
+
+        });
+
+        mAdapter = new SimpleAdapter(this, mPeopleList, R.layout.custcontview,
+                new String[] { "Name", "Phone", "Type" }, new int[] {
+                R.id.ccontName, R.id.ccontNo, R.id.ccontType });
+        mTxtPhoneNo.setAdapter(mAdapter);
 
 
         /*
@@ -272,6 +305,56 @@ public class MainActivity extends Activity {
         Log.e("","Not Found contact name");
 
         return addr;
+    }
+    public void PopulatePeopleList() {
+        mPeopleList.clear();
+        Cursor people = getContentResolver().query(
+                ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
+        while (people.moveToNext()) {
+            String contactName = people.getString(people
+                    .getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+            String contactId = people.getString(people
+                    .getColumnIndex(ContactsContract.Contacts._ID));
+            String hasPhone = people
+                    .getString(people
+                            .getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER));
+
+            if ((Integer.parseInt(hasPhone) > 0)){
+                // You know have the number so now query it like this
+                Cursor phones = getContentResolver().query(
+                        ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                        null,
+                        ContactsContract.CommonDataKinds.Phone.CONTACT_ID +" = "+ contactId,
+                        null, null);
+                while (phones.moveToNext()){
+                    //store numbers and display a dialog letting the user select which.
+                    String phoneNumber = phones.getString(
+                            phones.getColumnIndex(
+                                    ContactsContract.CommonDataKinds.Phone.NUMBER));
+                    String numberType = phones.getString(phones.getColumnIndex(
+                            ContactsContract.CommonDataKinds.Phone.TYPE));
+                    Map<String, String> NamePhoneType = new HashMap<String, String>();
+                    NamePhoneType.put("Name", contactName);
+                    NamePhoneType.put("Phone", phoneNumber);
+                    if(numberType.equals("0"))
+                        NamePhoneType.put("Type", "Work");
+                    else
+                    if(numberType.equals("1"))
+                        NamePhoneType.put("Type", "Home");
+                    else if(numberType.equals("2"))
+                        NamePhoneType.put("Type",  "Mobile");
+                    else
+                        NamePhoneType.put("Type", "Other");
+                    //Then add this map to the list.
+                    mPeopleList.add(NamePhoneType);
+                }
+                phones.close();
+            }
+        }
+        people.close();
+        startManagingCursor(people);
+
+
     }
 
 }
